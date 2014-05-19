9b0542090cc3cc7015a8cc0a24d7eb135386768b
Danilo Barbato <dbarbato@tv1.com.br>
2014-05-19 10:06:18 -0300

    Lista de usuários com query funcional

--- listausuario.php
+++ listausuario.php
@@ -2,21 +2,32 @@
 <!-- //TODO checar login e permissão 3 -->
 		  	<div class="container">
 			<h3> Lista usuários </h3>
+			<table class="table table-striped" width="647">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Nome</th>
+						<th>Usuário</th>
+						<th>Permissão</th>
+					</tr>
+				</thead>
+				<tbody>
 			<?php
-			// rascunho, acertar
-				$conexao->real_query("SELECT nome FROM usuarios ORDER BY nome ASC");
-				$resultado = $mysqli->use_result();
+				include 'restrito/conexao.php';
+				$resultado = $conexao->query("SELECT nome, usuario, permissao FROM usuarios");
+				$i = 1;
+				while ($linha = $resultado->fetch_assoc()){
+					echo '<tr>';
+					echo '<td>' . $i . '</td>'; // colocar número de repetição
+					echo '<td>' . $linha['nome'] .'</td>';
+					echo '<td>' . $linha['usuario'] .'</td>';
+					echo '<td>' . $linha['permissao'] .'</td>';
+					echo '</tr>';
+					$i++;
 
-				while ($linha = $resultado->fetch_assoc()) {
-					// criar tabela para exibir esses dados
-				   echo " Nome: " . $linha['nome'] . "\n";
-				   // imprimir nome, id, permissão
-				   // colocar botão para editar e remover usuários
-				   		// página de deletar deve ser incorporada nessa lista
-				   // colocar botão para adicionar usuário abaixo
 				}
-			?>
-				
+				?>
+					</tbody>
+				</table>
 		</div>	
-
 <?php include 'templates/footer.php' ?>
