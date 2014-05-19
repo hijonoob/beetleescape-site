39418f7deef850ed4650f817bbd07d6cb0186ecc
Danilo Barbato <dbarbato@tv1.com.br>
2014-05-19 10:15:02 -0300

    Chamando funções de edição e remoção de usuários

--- listausuario.php
+++ listausuario.php
@@ -9,12 +9,14 @@
 						<th>Nome</th>
 						<th>Usuário</th>
 						<th>Permissão</th>
+						<th>Editar</th>
+						<th>Remover</th>
 					</tr>
 				</thead>
 				<tbody>
 			<?php
 				include 'restrito/conexao.php';
-				$resultado = $conexao->query("SELECT nome, usuario, permissao FROM usuarios");
+					$resultado = $conexao->query("SELECT nome, usuario, permissao FROM usuarios");
 				$i = 1;
 				while ($linha = $resultado->fetch_assoc()){
 					echo '<tr>';
@@ -22,6 +24,8 @@
 					echo '<td>' . $linha['nome'] .'</td>';
 					echo '<td>' . $linha['usuario'] .'</td>';
 					echo '<td>' . $linha['permissao'] .'</td>';
+					echo '<td> <a href="editausuario.php?user='. $linha['usuario'].'">Editar</a></td>';
+					echo '<td> <a href="deletausuario.php?user='. $linha['usuario'].'">X</a></td>';
 					echo '</tr>';
 					$i++;
 
