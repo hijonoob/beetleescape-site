590401a39489a668c9398241306a4a9f96863681
Danilo Barbato <dbarbato@tv1.com.br>
2014-05-19 13:20:02 -0300

    Menu de jogador e remover login caso logado

--- templates/menu.php
+++ templates/menu.php
@@ -2,36 +2,20 @@
 	  <div class="container">
 	    <a class="navbar-brand" href="index.php">Beetle Escape</a>
 	      <ul class="nav navbar-nav">
-	      <!--
-	          <li>
-	              <a href="#">Downloads</a>
-	          </li>
-	          <li class="dropdown">
-	              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Media<b class="caret"></b></a>
-	              <ul class="dropdown-menu">
-	                <li>
-	                    <a href="#">Screenshots</a>
-	                </li>      
-	                <li>
-	                    <a href="#">Wallpapers</a>
-	                </li> 
-	                <li>
-	                    <a href="#">Press Kit</a>
-	                </li> 
-	              </ul>
-	          </li>
-	          <li>
-	              <a href="#">Rankings</a>
-	          </li>
-	          <li>
-	              <a href="#">FAQ</a>
-	          </li>
--->
 
-	          <!-- colocar condicional se logado aqui com exibição de links de usuário caso logado-->
+			<?php
+			  if(isset($_SESSION['permissao'])) {
+			      $permissao = $_SESSION['permissao'];
+			      if($permissao > 0) {
+			        include 'templates/menujogador.php';
+			      }
+			  } else {
+			  	echo '
 	          <li>
 	              <a href="login.php">Login</a>
-	          </li>
+	          </li>';
+	      	  }
+			?>
 	          
 	      </ul>
 	 </div>
---
+++ templates/menujogador.php
@@ -0,0 +1,26 @@
+	          <li>
+	              <a href="#">Downloads</a>
+	          </li>
+	          <li class="dropdown">
+	              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Media<b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li>
+	                    <a href="#">Screenshots</a>
+	                </li>      
+	                <li>
+	                    <a href="#">Wallpapers</a>
+	                </li> 
+	                <li>
+	                    <a href="#">Press Kit</a>
+	                </li> 
+	              </ul>
+	          </li>
+	          <li>
+	              <a href="#">Rankings</a>
+	          </li>
+	          <li>
+	              <a href="#">FAQ</a>
+	          </li>
+	          <li>
+	              <a href="#">Logout</a>
+	          </li>
