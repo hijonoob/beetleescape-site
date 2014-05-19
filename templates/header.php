f92cb887bfeec3065007b4f5fa01bc52b71f5f79
Danilo Barbato <dbarbato@tv1.com.br>
2014-05-19 16:56:25 -0300

    Adiciona TinyMCE para edição de notícias

--- templates/header.php
+++ templates/header.php
@@ -19,6 +19,15 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+<script type="text/javascript" src="tinymce/js/tinymce/tinymce.min.js"></script>
+
+<script type="text/javascript">
+tinyMCE.init({
+        mode : "textareas"
+});
+</script>
+
   </head>
   <body>
   	<div id="wrap">
