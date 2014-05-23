ea5c135bf35dec30fd249eab77416c00dc45b968
Mauricio Cardozo <mauricio.cardozo@me.com>
2014-05-23 15:42:56 -0300

    Homepage
    estilo novo da homepage, com jumbotron, fundo e imagens

--- index.php
+++ index.php
@@ -1,25 +1,52 @@
 <?php include 'templates/header.php' ?>
+
       </div>
-      <div class="jumbotron">
+      <div class="jumbotron" id="jumbo_chamada">
           <div class="container">
-              <h1>Beetle Escape</h1>
-              <p>O jogo que você jamais viu, e nunca verá.<br>
-                  Controle o besouro Carlos em sua jornada incessante para fugir do formigueiro das formigas malvadas!</p>
-              <a href="#" role="button" class="btn btn-lg btn-primary">Baixe agora, na Play Store</a>
+              <h1 class="be_logo">Beetle Escape</h1>
+              <p id="be_chamada">O jogo que você jamais viu, e nunca verá.<br>
+                  Controle o besouro Carlos em sua jornada incessante para fugir do formigueiro das formigas malvadas!
+              <a href="#" role="button" class="btn btn-lg btn-primary">Em breve, na Play Store</a>
+                  </p>
           </div>
       </div>
-      <div class="container">
-          <div class="row">
+
+    
+      <div class="jogo">
+          <div class="container">
+            <h2 class="header_image" id="be_jogo">O Jogo</h2>
               <div class="col-md-4"><h2>Pule!</h2>
-              Faça o besouro pular!
+                  <p class="be_texto">Faça o besouro pular!</p>
               </div>
-              <div class="col-md-4"><h2>Desça!</h2>
-              O besouro também pode descer!
+                  <div class="col-md-4"><h2>Desça!</h2>
+                    <p class="be_texto">O besouro também pode descer!</p>
+                  </div>
+                  <div class="col-md-4"><h2>Corra!</h2>
+                      <p class="be_texto">O besouro não para de correr!</p>
+                  </div>
               </div>
-              <div class="col-md-4"><h2>Corra!</h2>
-              O besouro não para de correr!
-              </div>
-          </div>
-      </div>
-      <div class="push"></div>
+    
+
+        <div class="container">
+        <h2 class="header_image" id="be_imagens">Imagens</h2>
+            <div class="col-md-4">
+                <img src="images/beetlenail.jpg">
+            </div>
+            <div class="col-md-8">
+                <h3>Baixe o wallpaper do Beetle Escape!</h3>
+                <a href="/images/paper/beetlepaper2560.png">2560x1440</a>
+                <a href="/images/paper/beetlepaper1920.png">1920x1080</a>
+                <a href="/images/paper/beetlepaper1280.png">1280x720</a>                     
+            </div>
+        </div>
+
+    
+
+        <div class="container">
+        <h2 class="header_image" id="be_faq">Dúvidas Frequentes</h2>
+            <h3>P: É só isso que vocês tem no site?</h3>
+            <p class="be_texto">Não, o nosso site tem muito mais! Cadastre-se agora para ter acesso a screenshots, mais wallpapers, suporte e batatas!</p>
+        </div>
+    </div>
+
 <?php include 'templates/footer.php' ?>
